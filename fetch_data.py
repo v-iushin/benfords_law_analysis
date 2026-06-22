482f31f268f4b510cae840a9fa36e2f59fc78a44
v-iushin <yvo.yushin@gmail.com>
2026-06-22 21:30:19 +0300

    Benford's Law: csv implementation

--- fetch_data.py
+++ fetch_data.py
@@ -1,17 +1,28 @@
 import requests
 import matplotlib.pyplot as plt
 import math as m
+from pathlib import Path
+import csv
 #import json
-#from pathlib import Path
 
 
-def data(url):
+
+def data_api(url):
     """Fetch data usgin URL"""
     r = requests.get(url, timeout=15)
     r.raise_for_status()
     resp = r.json()
     assert resp[0]["pages"] == 1, f"paginated: {resp[0]}"
     return resp
+
+
+
+def data_csv(path):
+    lines = path.read_text().splitlines()
+    reader = csv.reader(lines)
+    return reader
+
+
 
 def lead_digit(x):
     """Leading digit of number x"""
@@ -22,19 +33,45 @@ def lead_digit(x):
         x /= 10
     return int(x)
 
-def values_list(response, digits):
-    """Make list from data for each leading digit"""
-    values_digit = []
+def get_values_api(response):
+    """Collect values from response"""
+    values = []
     for i in range(len(response[1])):
         value = response[1][i]["value"]
+        values.append(value)
+    return values
+
+
+
+def get_values_csv(reader):
+    header_row = next(reader)
+    values = []
+    pop_index = header_row.index("population")
+    '''
+    for i, value in enumerate(header_row):
+        if value == "population":
+            pop_index = i
+    '''
+    for row in reader:
+        if row[pop_index] == "" or int(float(row[pop_index])) == 0:
+            continue
+        values.append(int(float(row[pop_index])))
+    return values
+
+
+
+def values_list(values, digits):
+    """Make list using values for each leading digit"""
+    values_digit = []
+    for value in values:
         if value is None or value == 0:
             continue
         values_digit.append(lead_digit(value))
     N = len(values_digit)
     print(f"Valid values N: {N}")
-    values = [values_digit.count(i) for i in digits]
-    print(f"Counts: {values}")
-    return values, N
+    counts = [values_digit.count(i) for i in digits]
+    print(f"Counts: {counts}")
+    return counts, N
 
 def benford(digits, N):
     """Theoretical Benford's value for specific total number of counts"""
@@ -58,17 +95,20 @@ def chi_sq(values, ben, digits):
 
 
 
-#BASE = Path(__file__).parent
+BASE = Path(__file__).parent
+path_cities = BASE/"data/worldcities.csv"
+reader_cities = data_csv(path_cities)
+
 #path_gdp = BASE/"data/gdp.json"
 #path_pop = BASE/"data/pop.json"
 
 url_gdp = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?format=json&per_page=20000&date=1960:2025"
-response_gdp = data(url_gdp)
+response_gdp = data_api(url_gdp)
 #readable_response_gdp = json.dumps(response_gdp, indent=4)
 #path_gdp.write_text(readable_response_gdp)
 
 url_pop = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json&per_page=20000&date=1960:2025"
-response_pop = data(url_pop)
+response_pop = data_api(url_pop)
 #readable_response_pop = json.dumps(response_pop, indent=4)
 #path_pop.write_text(readable_response_pop)
 
@@ -76,12 +116,20 @@ response_pop = data(url_pop)
 
 digits = list(range(1, 10))
 
-values_list_gdp, N_gdp = values_list(response_gdp, digits)
+values_cities = get_values_csv(reader_cities)
+values_list_cities, N_cities = values_list(values_cities, digits)
+benford_cities = benford(digits, N_cities)
+chi_cities = chi_sq(values_list_cities, benford_cities, digits)
+print()
+
+values_gdp = get_values_api(response_gdp)
+values_list_gdp, N_gdp = values_list(values_gdp, digits)
 benford_gdp = benford(digits, N_gdp)
 chi_gdp = chi_sq(values_list_gdp, benford_gdp, digits)
 print()
 
-values_list_pop, N_pop = values_list(response_pop, digits)
+values_pop = get_values_api(response_pop)
+values_list_pop, N_pop = values_list(values_pop, digits)
 benford_pop = benford(digits, N_pop)
 chi_pop = chi_sq(values_list_pop, benford_pop, digits)
 print()
@@ -100,3 +148,5 @@ ax.set_title("Benford's law vs observation")
 ax.set_xticks(digits)
 ax.legend()
 plt.show()
+
+# NO PLOT FOR CITIES FOR NOW
