d649ca3ac546d4aca94a61a263aa903e05462100
v-iushin <yvo.yushin@gmail.com>
2026-06-24 23:57:11 +0300

    Benford's Law: area data and sigma implementation

--- fetch_data.py
+++ fetch_data.py
@@ -80,6 +80,7 @@ def chi_sq(values, ben, digits):
 
 
 
+#
 def magn(x):
     return int(m.log10(x))
 
@@ -96,87 +97,148 @@ def magnitude_order(values):
                 magn_count += 1
         magns_count.append(magn_count)
     return magnitudes, magns_count
+#
+
+
+
+#
+def sigma_log10(values):
+    logs = [m.log10(abs(value)) for value in values]
+    mean = sum(logs) / len(logs)
+    sig = m.sqrt(sum((log - mean)**2 for log in logs) / len(logs))
+    print(f"Sigma: {round(sig, 2)}")
+    return sig
+#
 
 
 
 BASE = Path(__file__).parent
-path_cities = BASE/"data/worldcities.csv"
-reader_cities = data_csv(path_cities)
-
-#path_gdp = BASE/"data/gdp.json"
-#path_pop = BASE/"data/pop.json"
 
 url_gdp = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?format=json&per_page=20000&date=1960:2025"
 response_gdp = data_api(url_gdp)
 #readable_response_gdp = json.dumps(response_gdp, indent=4)
+#path_gdp = BASE/"data/gdp.json"
 #path_gdp.write_text(readable_response_gdp)
 
 url_pop = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json&per_page=20000&date=1960:2025"
 response_pop = data_api(url_pop)
 #readable_response_pop = json.dumps(response_pop, indent=4)
+#path_pop = BASE/"data/pop.json"
 #path_pop.write_text(readable_response_pop)
+
+#
+url_area = "https://api.worldbank.org/v2/country/all/indicator/AG.LND.TOTL.K2?format=json&per_page=300&date=2020"
+response_area = data_api(url_area)
+#
+
+path_cities = BASE/"data/worldcities.csv"
+reader_cities = data_csv(path_cities)
 
 
 
 digits = list(range(1, 10))
 
-values_cities = get_values_csv(reader_cities)
-values_digit_cities, N_cities = values_digit(values_cities, digits)
-benford_cities = benford(digits, N_cities)
-chi_cities = chi_sq(values_digit_cities, benford_cities, digits)
-print()
-
+print("GDP, countires:")
 values_gdp = get_values_api(response_gdp)
 values_digit_gdp, N_gdp = values_digit(values_gdp, digits)
 benford_gdp = benford(digits, N_gdp)
 chi_gdp = chi_sq(values_digit_gdp, benford_gdp, digits)
+sigma_gdp = sigma_log10(values_gdp)
 print()
 
+print("Population, countries:")
 values_pop = get_values_api(response_pop)
 values_digit_pop, N_pop = values_digit(values_pop, digits)
 benford_pop = benford(digits, N_pop)
 chi_pop = chi_sq(values_digit_pop, benford_pop, digits)
+sigma_pop = sigma_log10(values_pop)
+print()
+
+#
+print("Area, countries:")
+values_area = get_values_api(response_area)
+values_digit_area, N_area = values_digit(values_area, digits)
+benford_area = benford(digits, N_area)
+chi_area = chi_sq(values_digit_area, benford_area, digits)
+sigma_area = sigma_log10(values_area)
+print()
+#
+
+print("Population, cities:")
+values_cities = get_values_csv(reader_cities)
+values_digit_cities, N_cities = values_digit(values_cities, digits)
+benford_cities = benford(digits, N_cities)
+chi_cities = chi_sq(values_digit_cities, benford_cities, digits)
+sigma_cities = sigma_log10(values_cities)
+print()
+print()
 print()
 
 
-
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(15,5))
 width = 0.3
 ax1.bar([d + width/2 for d in digits], values_digit_gdp, width, color="blue", alpha=0.5, label="GDP, countires")
 ax1.bar([d - width/2 for d in digits], values_digit_pop, width, color="orange", alpha=0.5, label="Population, countries")
-ax1.plot(digits, benford_gdp, marker="o", color="blue", label="GDP, counties (benford)")
+ax1.plot(digits, benford_gdp, marker="o", color="blue", label="GDP, countries (benford)")
 ax1.plot(digits, benford_pop, marker="o", color="orange", label="Population, countries (benford)")
 ax1.set_xlabel("Leading digit")
 ax1.set_ylabel("Counts")
 ax1.set_title("Benford's law vs observation")
 ax1.set_xticks(digits)
 ax1.legend()
-ax2.bar(digits, values_digit_cities, width, color="green", alpha=0.5, label="Population, cities")
-ax2.plot(digits, benford_cities, marker="o", color="green", label="Population, cities (benford)")
+#
+ax2.bar(digits, values_digit_area, width, color="black", alpha=0.5, label="Area, countries")
+ax2.plot(digits, benford_area, marker="o", color="black", label="Area, countries (benford)")
 ax2.set_xlabel("Leading digit")
 ax2.set_ylabel("Counts")
 ax2.set_title("Benford's law vs observation")
 ax2.set_xticks(digits)
 ax2.legend()
+#
+ax3.bar(digits, values_digit_cities, width, color="green", alpha=0.5, label="Population, cities")
+ax3.plot(digits, benford_cities, marker="o", color="green", label="Population, cities (benford)")
+ax3.set_xlabel("Leading digit")
+ax3.set_ylabel("Counts")
+ax3.set_title("Benford's law vs observation")
+ax3.set_xticks(digits)
+ax3.legend()
 plt.show()
 
 
 
-magns_cities, magns_count_cities = magnitude_order(values_cities)
-print(magns_cities)
-print(magns_count_cities)
-print()
+print("GDP, countires:")
 magns_gdp, magns_count_gdp = magnitude_order(values_gdp)
 print(magns_gdp)
 print(magns_count_gdp)
 print()
+
+print("Population, countries:")
 magns_pop, magns_count_pop = magnitude_order(values_pop)
 print(magns_pop)
 print(magns_count_pop)
 print()
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(17,5))
-ax1.bar(magns_cities, magns_count_cities)
-ax2.bar(magns_gdp, magns_count_gdp)
-ax3.bar(magns_pop, magns_count_pop)
+#
+print("Area, countries:")
+magns_area, magns_count_area = magnitude_order(values_area)
+print(magns_area)
+print(magns_count_area)
+print()
+#
+
+print("Population, cities:")
+magns_cities, magns_count_cities = magnitude_order(values_cities)
+print(magns_cities)
+print(magns_count_cities)
+print()
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=1, ncols=4, figsize=(16,4))
+ax1.bar(magns_gdp, magns_count_gdp)
+ax2.bar(magns_pop, magns_count_pop)
+ax3.bar(magns_area, magns_count_area)
+ax4.bar(magns_cities, magns_count_cities)
 plt.show()
+
+
+
+print()
